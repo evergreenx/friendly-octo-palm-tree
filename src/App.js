7b32d7396eccd0ecd71c97c715fcf23f0517d22b
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-05-27 00:30:00 +0100

    adad

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import "./App.css";
 import Welcome from "./components/Welcome";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
-
-import ReactGA from "react-ga4";
-
-ReactGA.initialize([
-  {
-    trackingId: "G-ZQ3LB5HZ6F",
-  },
-]);
+import GA4React from "ga-4-react";
 
 function App() {
+  try {
+    setTimeout((_) => {
+      const ga4react = new GA4React("G-ZQ3LB5HZ6F");
+      ga4react.initialize().catch((err) => console.error(err));
+    }, 4000);
+  } catch (err) {
+    console.error(err);
+  }
   return (
     <div className="app">
       <Router>
